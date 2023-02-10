209ceb60205354c2c7082540528720d81bc6c62a
UO263728 <UO263728@uniovi.es>
2023-02-10 12:08:41 +0100

    SDI-2223-505-2.2-Trabajando con Controladores.

---
+++ src/main/java/com/uniovi/sdi2223505spring/controllers/MarksController.java
@@ -0,0 +1,26 @@
+package com.uniovi.sdi2223505spring.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import com.uniovi.sdi2223505spring.entities.Mark;
+
+@RestController
+public class MarksController {
+
+    @RequestMapping("/mark/list")
+    public String getList() {
+        return "Getting List";
+    }
+
+    @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+    public String setMark(@ModelAttribute Mark mark) {
+        return "added: " + mark.getDescription()
+                + " with score: " + mark.getScore()
+                + " id: " + mark.getId();
+    }
+
+    @RequestMapping("/mark/details/{id}")
+    public String getDetail(@PathVariable Long id) {
+        return "Getting Details =>" + id;
+    }
+
+}
