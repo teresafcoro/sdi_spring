8b430e1dcd0f3ebec8ac82efd3d2e49ba935aa25
UO263728 <UO263728@uniovi.es>
2023-02-10 11:39:10 +0100

    SDI-2223-505-2.1-Create Spring project.

---
+++ src/test/java/com/uniovi/sdi2223505spring/Sdi2223505SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223505spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223505SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
