4d4c2c15328c3e4899ebfa29ef899b0ae536ed7f
UO263728 <UO263728@uniovi.es>
2023-02-17 12:15:02 +0100

    SDI-2223-505-3.2-Internacionalización.

---
+++ src/main/java/com/uniovi/sdi2223505spring/CustomConfiguration.java
@@ -0,0 +1,36 @@
+package com.uniovi.sdi2223505spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class CustomConfiguration  implements WebMvcConfigurer {
+
+    // Defino los Beans necesarios para la gestión del idioma y el método addInterceptors.
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es", "ES"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang"); return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
---
+++ src/main/resources/static/script/languages.js
@@ -0,0 +1,12 @@
+// Gestiona los idiomas soportados en la aplicación
+$(document).ready(function () {
+    $("#languageDropdownMenuButton a").click(function (e) {
+        e.preventDefault(); // cancel the link behaviour
+        var languageSelectedText = $(this).text();
+        var languageSelectedValue = $(this).attr("value");
+
+        $("#btnLanguage").text(languageSelectedText);
+        window.location.replace('?lang=' + languageSelectedValue);
+        return false;
+    });
+});
